7fac517319166188906c3281ccb6a1cbc74a4c7a
Giovanny trotta <giovannytrotta13@gmail.com>
2025-11-29 12:10:31 +0100

    agregado scrit de google tag manager configurado con pixel falta microsoft

--- app/layout.js
+++ app/layout.js
@@ -117,6 +117,28 @@ export default function RootLayout({ children }) {
     src={`https://www.facebook.com/tr?id=1187322646119344&ev=PageView&noscript=1`}
   />
 </noscript>
+
+{/* Google Tag Manager Script*/}
+
+<Script id="google-tag-manager" strategy="afterInteractive">
+  {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-P8S5NSL2');
+  `}
+</Script>
+
+
+<noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-P8S5NSL2"
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+  />
+</noscript>
       </head>
       <body className="antialiased">
         {children}
